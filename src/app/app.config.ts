cf9c7b1cfd1e9159d53d84aeea2531f68871bb39
Vidiic <jordividal1997@gmail.com>
2025-02-12 19:54:28 +0100

    add ssr

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -7,11 +7,12 @@ import { provideRouter, withComponentInputBinding } from '@angular/router';
 
 import { routes } from './app.routes';
 import { provideHttpClient, withFetch } from '@angular/common/http';
+import { provideClientHydration, withEventReplay } from '@angular/platform-browser';
 
 export const appConfig: ApplicationConfig = {
   providers: [
     provideExperimentalZonelessChangeDetection(),
     provideRouter(routes, withComponentInputBinding()),
-    provideHttpClient(withFetch()),
+    provideHttpClient(withFetch()), provideClientHydration(withEventReplay()),
   ],
 };
